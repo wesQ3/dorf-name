4699b122394a662a84fa4e4511302d0c78b5fea0
Wes Malone <wesQ3@users.noreply.github.com>
2024-09-06 08:23:47 -0500

    Use vec for future random selection

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
-use std::collections::HashMap;
+use std::fmt;
 use std::fs::File;
-use std::io::prelude::*;
 use std::io::BufReader;
+use std::io::prelude::*;
+
+struct Word {
+    lang_en: String,
+    lang_dw: String,
+}
+
+impl fmt::Display for Word {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} => {}", self.lang_en, self.lang_dw)
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let file_name = "data/language_DWARF.txt"; // utf8 converted
     let f = File::open(file_name)?;
     let reader = BufReader::new(f);
 
-    let mut tl_map = HashMap::new();
+    let mut word_list = Vec::new();
     for line in reader.lines() {
         let line = line?;
         if let Some(word) = process_line(line.clone()) {
-            tl_map.insert(word.0, word.1);
+            let word = Word { lang_en: word.0, lang_dw: word.1 };
+            word_list.push(word);
         } else {
             println!("skipped {}", line);
         }
     }
-    println!("parsed {} words", tl_map.keys().len());
+    println!("parsed {} words", word_list.len());
+    println!("{}", word_list[1]);
+    println!("{}", word_list[10]);
+    println!("{}", word_list[100]);
     Ok(())
 }
 
